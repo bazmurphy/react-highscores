13cad9a6b1a0ca265657e422c353638c3cd2bbcb
Baz Murphy <bazmurphy@gmail.com>
2022-08-29 22:25:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import allCountryScores from './allCountryScores';
 
 function App() {
+
+  const [data, setData] = React.useState(allCountryScores);
+  const [countryOrder, setCountryOrder] = React.useState("Ascending");
+  const [scoresOrder, setScoresOrder] = React.useState("Descending");
+
+  const changeCountryOrder = () => {
+    if (countryOrder === "Ascending") {
+      setData((prevData) => prevData.sort((elementOne, elementTwo) => elementTwo.name.localeCompare(elementOne.name)));
+      setCountryOrder("Descending");
+    } else if (countryOrder === "Descending") {
+      setData((prevData) => prevData.sort((elementOne, elementTwo) => elementOne.name.localeCompare(elementTwo.name)));
+      setCountryOrder("Ascending");
+    }
+  };
+  
+  const changeScoreOrder = () => {
+    if (scoresOrder === "Descending") {
+      setData((prevData) => prevData.map(element => element.scores.sort((elementOne, elementTwo) => elementOne.s - elementTwo.s)));
+      setScoresOrder("Ascending");
+    } else if (scoresOrder === "Ascending") {
+      setData((prevData) => prevData.map(element => element.scores.sort((elementOne, elementTwo) => elementTwo.s - elementOne.s)));
+      setScoresOrder("Descending");
+    }
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="change-country-order-button" onClick={changeCountryOrder}>Change Country Order<br></br>Currently: {countryOrder}</button>
+      <button className="change-scores-order-button" onClick={changeScoreOrder}>Change Scores Order<br></br>Currently: {scoresOrder}</button>
+      <header>High Scores per Country</header>
+      {data.map(element => {
+          return (
+            <div className="country-scores-container">
+                <div className="country-scores-title">HIGH SCORES: {element.name}</div>
+                {element.scores.map(element => {
+                      return (
+                        <div className="country-scores-row">
+                          <span className="country-scores-name">{element.n}</span>
+                          <span className="country-scores-number">{element.s}</span>
+                        </div>
+                      )
+                  })
+                }
+            </div>
+          );
+        })
+      }
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/testing.js
@@ -0,0 +1,87 @@
+let allCountryScores = [
+    {
+      name: "Ethiopia", 
+      scores:  [ {n: "Sub", s: 9990}, {n: "lucy", s: 4134234}, {n: "DWH", s: 9999},  {n: "Hanif", s: 999999999} ]
+    },     
+    {
+      name: "Scotland", 
+      scores: [  {n: "groundkeeper willie", s: 4000}, {n: "Neill", s: 999999}, {n: "braveheart", s: -200}]
+    },{
+      name: "England", 
+      scores: [ {n: "Jonny", s: 202020}, {n: "Chris", s: 202021}]
+    },{
+      name: "Brazil", 
+      scores: [ {n: "Mozart", s: 999}]
+    },
+    {
+      name: "Colombia", 
+      scores: [ {n: "Maria", s: 6000}, {n: "Melanie", s: 99999999}, {n: "Ali", s: 5000}]
+    }, 
+    {
+      name: "Turkey", 
+      scores: [ {n: "selim", s: 900  }, {n: "mahmut", s: 1000  }, {n: "morat", s: 999  } ]
+    },
+    {
+      name: "Iran", 
+      scores: [ {n: "arosha", s: 5550  },  {n: "zahra", s: 3000  }, {n: "nader", s: 2000  }, {n: "Bani", s: 1999  } ]
+    },
+    {
+      name: "Bangladesh", 
+      scores: [ {n: "rahman", s: 700200}, {n: "rayhan", s: 18238123}, {n: "ali", s: 5400000} ]
+    },
+];
+
+const changeCountryOrder = (array, order) => {
+  if (order === "Ascending") {
+    return array.sort((elementOne, elementTwo) => elementOne.name.localeCompare(elementTwo.name));
+  } else if (order === "Descending") {
+    return array.sort((elementOne, elementTwo) => elementTwo.name.localeCompare(elementOne.name));
+  }
+};
+
+const changeScoreOrder = (array, order) => {
+  if (order === "Descending") {
+    return array.map(element => element.scores.sort((elementOne, elementTwo) => elementTwo.s - elementOne.s));
+  } else if(order === "Ascending") {
+    return array.map(element => element.scores.sort((elementOne, elementTwo) => elementOne.s - elementTwo.s));
+  }
+};
+
+console.log("\n COUNTRIES ASCENDING")
+console.log(changeCountryOrder(allCountryScores, "Ascending"));
+console.log("\n COUNTRIES DESCENDING")
+console.log(changeCountryOrder(allCountryScores, "Descending"));
+console.log("\n SCORES ASCENDING")
+console.log(changeScoreOrder(allCountryScores, "Ascending"));
+console.log("\n SCORES DESCENDING")
+console.log(changeScoreOrder(allCountryScores, "Descending"));
+
+
+
+// array.sort((a, b) => {
+//     if (a.name === b.name) return 0;
+//     return a.name > b.name ? 1 : -1;
+//   });
+
+// const allCountryScoresSortedAlphabetically = allCountryScores.sort((a,b) => {
+//     return a.name.toLowerCase() === b.name.toLowerCase() ? 
+//                 0 : a.name.toLowerCase() > b.name.toLowerCase() ? 
+//                     1 : -1;
+// });
+
+// const result1 = allCountryScores.sort((a, b) => a.name.localeCompare(b.name));
+// console.log(result1);
+
+// IN REACT IT WILL BE NESTED:
+
+// const test = allCountryScores.map(element => {
+    
+//     element.scores.map(element => {
+
+//     })
+
+// });
+
+// const test2 = { scores: [ {n: "rahman", s: 700200}, {n: "rayhan", s: 18238123}, {n: "ali", s: 5400000} ] }
+// const result2 = test2.scores.sort((elementOne,elementTwo) => elementTwo.s - elementOne.s)
+// console.log(result2);
