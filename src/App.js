4f7174f42a381712d4b64c289a816cb42bfd4261
Baz Murphy <bazmurphy@gmail.com>
2022-09-04 15:58:04 +0100

    initialise scoresOrder as none by default

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [scores, setScores] = React.useState(allCountryScores);
 
   const [countryOrder, setCountryOrder] = React.useState("None");
-  const [scoresOrder, setScoresOrder] = React.useState("Descending");
+  const [scoresOrder, setScoresOrder] = React.useState("None");
 
   // toggle to display Country Names alphabetically ascending or descending
   const changeCountryOrder = () => {
@@ -22,26 +22,26 @@ function App() {
 
   // toggle to display Scores numerically descending or ascending
   const changeScoreOrder = () => {
-    if (scoresOrder === "Descending") {
-      // setScores(prevScores => prevScores.map(element => element.scores.sort((elementOne, elementTwo) => elementTwo.s - elementOne.s)));
-        // Uncaught TypeError: element.scores is undefined
-        // nested sort doesn't seem to work?
-      setScores(prevScores => prevScores.map(element => {
-        element.scores.sort((elementOne, elementTwo) => elementTwo.s - elementOne.s)
-        return element;
-      }));
-      // ^ THANK YOU Berkeli
-      setScoresOrder("Ascending");
-    } else if (scoresOrder === "Ascending") {
+    if (scoresOrder === "Ascending" || scoresOrder === "None") {
       // setScores((prevScores) => prevScores.map(element => element.scores.sort((elementOne, elementTwo) => elementOne.s - elementTwo.s)));
-        // Uncaught TypeError: element.scores is undefined
-        // nested sort doesn't seem to work?
+      // Uncaught TypeError: element.scores is undefined
+      // nested sort doesn't seem to work?
       setScores(prevScores => prevScores.map(element => {
         element.scores.sort((elementOne, elementTwo) => elementOne.s - elementTwo.s)
         return element;
       }));
       // ^ THANK YOU Berkeli
       setScoresOrder("Descending");
+    } else if (scoresOrder === "Descending") {
+      // setScores(prevScores => prevScores.map(element => element.scores.sort((elementOne, elementTwo) => elementTwo.s - elementOne.s)));
+      // Uncaught TypeError: element.scores is undefined
+      // nested sort doesn't seem to work?
+      setScores(prevScores => prevScores.map(element => {
+        element.scores.sort((elementOne, elementTwo) => elementTwo.s - elementOne.s)
+        return element;
+      }));
+      // ^ THANK YOU Berkeli
+      setScoresOrder("Ascending");
     }
   };
 
@@ -50,7 +50,7 @@ function App() {
 
       <button className="change-country-order-button" onClick={changeCountryOrder}>Change Country Order<br></br>Currently: {countryOrder}</button>
       <button className="change-scores-order-button" onClick={changeScoreOrder}>Change Scores Order<br></br>Currently: {scoresOrder}</button>
-      
+
       <header>High Scores per Country</header>
 
       {scores
@@ -59,25 +59,25 @@ function App() {
         .map(element => {
           return (
             <div className="country-scores-container">
-                <div className="country-scores-title">HIGH SCORES: {element.name}</div>
+              <div className="country-scores-title">HIGH SCORES: {element.name}</div>
 
-                {element.scores
-                      // .sort((elementOne, elementTwo) => elementOne.s - elementTwo.s)
-                      // ^ this works when i do it before .map in the render here (but yes its chained not nested)
-                      .map(element => {
-                      return (
-                        <div className="country-scores-row">
-                          <span className="country-scores-name">{element.n}</span>
-                          <span className="country-scores-number">{element.s}</span>
-                        </div>
-                      )
-                  })
-                }
+              {element.scores
+                // .sort((elementOne, elementTwo) => elementOne.s - elementTwo.s)
+                // ^ this works when i do it before .map in the render here (but yes its chained not nested)
+                .map(element => {
+                  return (
+                    <div className="country-scores-row">
+                      <span className="country-scores-name">{element.n}</span>
+                      <span className="country-scores-number">{element.s}</span>
+                    </div>
+                  )
+                })
+              }
 
             </div>
           )
         })
-      }   
+      }
     </div>
   );
 };
